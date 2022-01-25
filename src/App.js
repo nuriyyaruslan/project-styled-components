7d1764faea37b9004280bead0dec088302790975
nuriyyaruslan <nuriyya.isgandarova@gmail.com>
2022-01-25 23:03:07 +0400

    25 yanvar update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,58 @@
+import {ThemeProvider, createGlobalStyle} from 'styled-components';
+import logo from './logo.svg';
 import './App.css';
-import StyledButton, { FancyButton } from './components/Button/Button';
+import './styles.css'
+import StyledButton, { 
+  FancyButton,
+  SubmitButton 
+} from './components/Button/Button';
+import { AnimatedLogo, DarkButton } from './components/Button/Button.styles'
+
+const theme = {
+  dark: {
+    primary: '#000',
+    text: '#fff'
+  },
+  light: {
+    primary: '#fff',
+    text: '#000'
+  },
+  fontFamily: 'Segoe UI'
+}
+const GlobalStyle = createGlobalStyle`
+  button {
+    font-family: ${(props) => props.theme.fontFamily};
+  }
+`
 
 function App() {
   return (
-    <div className="App">
-      <h1>Learning styled components</h1>
-      <StyledButton>Styled Button</StyledButton>
-      <div>
-        <br />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle/>
+      <div className="App">
+        <h1>Learning styled components</h1>
+        {/* <img src={logo} className='App-logo' alt="logo" /> */}
+        <AnimatedLogo src={logo} />
+        <StyledButton type='submit'>Styled Button</StyledButton>
+        <div>
+          <br />
+        </div>
+        <StyledButton variant='outline'>Styled Button</StyledButton>
+        <div>
+          <br />
+        </div>
+        {/* as="a" */}
+        <FancyButton>Fancy Button</FancyButton>
+        <div>
+          <br />
+        </div>
+        <SubmitButton>Submit</SubmitButton>
+        <div>
+          <br />
+        </div>
+        <DarkButton>Dark theme</DarkButton>
       </div>
-      <StyledButton variant='outline'>Styled Button</StyledButton>
-      <div>
-        <br />
-      </div>
-      {/* as="a" */}
-      <FancyButton>Fancy Button</FancyButton>
-    </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/Button/Button.styles.js
+++ src/components/Button/Button.styles.js
@@ -1,4 +1,4 @@
-import styled from "styled-components"
+import styled, {keyframes} from "styled-components"
 
 export const StyledButton = styled.button`
   border:2px solid orangered;
@@ -12,9 +12,46 @@ export const StyledButton = styled.button`
   border-radius: 8px;
   cursor:pointer;
   transition:all .5s ease-in-out;
+  //pseudo classes
+  &:hover {
+    background-color:${(props) => props.variant !== 'outline' ? 'transparent' : 'orangered'};
+    color:${(props) => props.variant !== 'outline' ? 'orangered' : '#fff'};
+  }
 `
-
+// extending styles
 export const FancyButton = styled(StyledButton)`
   background-image: linear-gradient(to right, #f6d365 0%, #fda085 100%);
   border:none;
 `
+// passed props and adding attributes
+export const SubmitButton = styled(StyledButton).attrs(props => ({
+  type: 'submit',
+}))`
+  box-shadow: 0 9px #999;
+  &:active {
+    background-color: ${(props) => props.variant !== 'outline' ? '#fff' : '#orangered'};
+    box-shadow: 0 5px #666;
+    transform: translate(4px);
+  }
+`
+
+const rotate = keyframes`
+  from{
+    transform: rotate()(0deg);
+  }
+  to{
+    transform: rotate(360deg);
+  }
+`
+export const AnimatedLogo = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+  animation: ${rotate} infinite 20s linear;
+ `
+export const DarkButton = styled(StyledButton)`
+  border:2px solid ${(props) => props.theme.dark.primary};
+  background-color:${(props) => props.theme.dark.primary};
+  color:${(props) => props.theme.light}
+`
+
+//global styles
